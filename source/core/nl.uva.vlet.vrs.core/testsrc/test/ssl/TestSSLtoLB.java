432243f1807d1c8a7607d565802817c672c59e29
devpiter <devpiter@796de75e-ed0f-4de0-881b-f1dc9bee7bff>
2012-05-22 18:33:36 +0000

    imports

--- source/core/nl.uva.vlet.vrs.core/testsrc/test/ssl/TestSSLtoLB.java
+++ source/core/nl.uva.vlet.vrs.core/testsrc/test/ssl/TestSSLtoLB.java
@@ -3,11 +3,7 @@ package test.ssl;
 import java.io.InputStream;
 import java.net.Socket;
 import java.net.URL;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.cert.CertificateException;
 
-import nl.uva.vlet.exception.VlException;
 import nl.uva.vlet.net.ssl.CertificateStore;
 import nl.uva.vlet.net.ssl.SslUtil;
 
